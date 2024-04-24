a103cfdee905c2496c32252c460e6c8b4a8a7c9f
marveal <46985974+marveal-rab@users.noreply.github.com>
2024-04-24 22:03:49 +0800

    add kubo client

--- contexts/ipfs-provider.tsx
+++ contexts/ipfs-provider.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import React from "react";
+import { create } from "kubo-rpc-client";
 
 interface IPFSContextType {}
 
 export const IPFSContext = React.createContext<IPFSContextType>({});
 
 export const IPFSProvider = ({ children }: { children: React.ReactNode }) => {
-  // const ipfs = createHelia({
-  //   host: "localhost",
-  //   port: 5001,
-  //   protocol: "http",
-  // });
+  const ipfs = create({
+    host: "localhost",
+    port: 5001,
+    protocol: "http",
+  });
 
   return <IPFSContext.Provider value={{}}>{children}</IPFSContext.Provider>;
 };
